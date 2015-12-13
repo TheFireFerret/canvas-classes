b01b3621d7c15783f76410704a5f2325456ad1f9
TheFireFerret <SeaLion@Sea-Lion.local>
2015-12-13 01:30:09 -0600

    webstorm is nice
    idk! now how do i add and store links
    also auto clicking buttons??

---
+++ CanvasClasses.safariextension/scripts.js
@@ -0,0 +1,17 @@
+function displaylinks() {
+    var links = new Array();
+    links[0]="OS";
+    links[1]="Data Management";
+    links[2]="Stat";
+    links[3]="LAS";
+
+    var URLs = new Array();
+    URLs[0]="https://utexas.instructure.com/courses/1144197";
+    URLs[1]="https://utexas.instructure.com/courses/1144118";
+    URLs[2]="https://utexas.instructure.com/courses/1154537";
+    URLs[3]="https://utexas.instructure.com/courses/1147037";
+
+     for (var i = 0; i < links.length; i++) {
+         document.getElementById('links').innerHTML += (links[i].link(URLs[i]) + "<br>");
+     }
+}
